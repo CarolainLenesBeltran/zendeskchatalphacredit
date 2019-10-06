75dcde56ef5001cad5d578ab879af341553bf0a3
Carolain Lenes <carolain.lenes@alphacredit.co>
2019-10-05 21:25:44 -0500

    Integraion Zendesk

--- app/src/main/java/com/example/chattest/Controller/MainActivity.java
+++ app/src/main/java/com/example/chattest/Controller/MainActivity.java
@@ -3,14 +3,39 @@ package com.example.chattest.Controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.chattest.R;
 
+import zendesk.core.AnonymousIdentity;
+import zendesk.core.Identity;
+import zendesk.core.Zendesk;
+import zendesk.support.Support;
+import zendesk.support.request.RequestActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_VIEWPAGER_POSITION = "extra_viewpager_pos";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Zendesk.INSTANCE.init(this, getResources().getString(R.string.zd_url),
+                getResources().getString(R.string.zd_appid),
+                getResources().getString(R.string.zd_oauth));
+
+        Identity identity = new AnonymousIdentity();
+        Zendesk.INSTANCE.setIdentity(identity);
+
+        Support.INSTANCE.init(Zendesk.INSTANCE);
+    }
+
+    public void openActivity(View view){
+        RequestActivity.builder()
+                .withRequestSubject("Testing Support SDK 3.0")
+                .withTags("sdk", "android")
+                .show(this);
     }
 }
